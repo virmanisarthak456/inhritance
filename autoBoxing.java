a6d50ce955c980b757be6bb3658a675482156872
Sarhak Virmani <virmanisarthak456@gmail.com>
2024-11-11 23:54:37 +0530

    auto boxing and auto unboxing

---
+++ autoBoxing.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class autoBoxing {
+
+    public static void main(String[] args) {
+        int num = 5;
+        Integer num1 = num; // auto-boxing , converting non-primirtive datatype to primitive data type
+
+        int num2 = num1; // auto-unboxing , converting primitive to non-primitive data type
+    }
+
+}
